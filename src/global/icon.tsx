b8780498807a845fd880192737ebc8e393ddf4d9
Mikeps2 <mikeps.dev@gmail.com>
2025-02-26 12:26:54 -0300

    pages

---
+++ src/global/icon.tsx
@@ -0,0 +1,3 @@
+import icon from "../../assets/icon.png";
+
+export default{icon};
---
+++ src/global/themes.tsx
@@ -0,0 +1,56 @@
+export const themes = {
+    colors:{
+        verdeEscuro: "rgb(0, 53, 14)",
+        verdeEscuro5: "rgba(0, 53, 14, 0.5)",
+        verdeClaro: "#rgb(167, 234, 157)",
+        verdeClaro75: "rgba(167, 234, 157, 0.75)",
+        branco5: "rgba(255, 255, 255, 0.5)",
+        branco8: "#rgba(255, 255, 255, 0.8)",
+    },
+
+    fonts:{
+        jaro: 'Jaro-Regular',
+        podkova: 'Podkova-Regular',
+    },
+    
+    strings: {
+        gmail: "Entrar com Gmail",
+        appleID: "Entrar com Apple ID", 
+
+        agendarHora: "Agendar Horário", 
+        agendamentos: "Agendamentos", 
+        logout: "Logout", 
+        
+        agendar: "Agendar",
+        cancelarAge: "Cancelar Agendamento",
+
+        agendCancelado: "Agendamento cancelado\ncom sucesso.",
+        agendEfetuado: "Agendamento efetuado\ncom sucesso.",
+        confirmar: "Ok",
+        
+        textHorarios: "Horários", 
+    },
+
+    data:{
+        1: "Janeiro,",
+        2: "Fevereiro,",
+        3: "Março,",
+        4: "Abril,",
+        5: "Maio,",
+        6: "Junho,",
+        7: "Julho,",
+        8: "Agosto,",
+        9: "Setembro,",
+        10: "Outubro,",
+        11: "Novembro,",
+        12: "Dezembro,",
+
+        dom: "Dom", 
+        seg: "Seg", 
+        ter: "Ter", 
+        qua: "Qua", 
+        qui: "Qui", 
+        sex: "Sex",
+        sab: "Sáb",
+    }
+}
